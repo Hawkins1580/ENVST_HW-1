000e6d1b5772f71615babf4f9a8e561703244a9f
hawkinsnick <nhawkins@hamilton.edu>
2022-01-27 16:44:18 +0000

    started the homework

--- HW#1.R
+++ HW#1.R
@@ -1,2 +1,36 @@
 # Activity & HW #1
 
+# Examples of calculations
+6*6
+6/6
+6^2
+
+# Prompt #1
+snow_depth <- c(2.5, 3, 5, 4.5)
+
+# Snow depth in inches
+snow_depth*2.54
+
+
+# Prompt #2
+my_DataFrame <- data.frame(Peak_Name =c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak"),
+                           Elevation =c(4960, 4926, 4857, 4840),
+                           Climb_Ascent =c(3570, 4265, 2800, 4178),
+                           Round_Trip =c(17.7, 17.9, 13.2, 16))
+
+# Homework
+
+
+# Question #1
+# Converting elevation to meters
+my_DataFrame[,2]*0.3048
+
+
+# Question #2
+# * In Word Doc *
+
+
+# Question #3
+# Isolating longest round trip length 
+my_DataFrame[2,4]
+
